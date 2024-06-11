5e693e1036135731434f449260696e578ff3391c
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-11 11:56:47 +0545

    feat: Add AppModule and AuthApi for dependency injection and network requests

---
+++ AuthPrototype/app/src/main/java/me/axm/auth_prototype/AppModule.kt
@@ -0,0 +1,31 @@
+package me.axm.auth_prototype
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import me.axm.auth_prototype.auth.AuthRepository
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideAuthApi(): AuthApi {
+        return Retrofit.Builder()
+            .baseUrl("http://192.168.0.2:8080/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(api: AuthApi, prefs: SharedPreferences): AuthRepository {
+        return AuthRepositoryImpl(api, prefs)
+    }
+}
